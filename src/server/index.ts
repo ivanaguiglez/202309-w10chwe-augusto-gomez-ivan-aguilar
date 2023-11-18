bd7e393e484f72817196c67c194169c77419299e
Ivan Aguilar <ivanaguiglez@gmail.com>
2023-11-18 12:31:02 +0100

    Fix helmet protection to app

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,8 @@
-import helmet from "helmet";
+import morgan from "morgan";
 import app from "./app.js";
 import endpointNotFound from "./middlewares/errors/endPointNotFound.js";
-import morgan from "morgan";
 import pingRouter from "../features/ping/router/pingRouter.js";
 
-app.use(helmet());
 app.use(morgan("dev"));
 app.use("/", pingRouter);
 app.use(endpointNotFound);
