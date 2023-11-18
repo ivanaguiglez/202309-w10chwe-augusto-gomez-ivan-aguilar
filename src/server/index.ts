001b4b52d84896102f88d9f4d09063293bbdd3f1
Ivan Aguilar <ivanaguiglez@gmail.com>
2023-11-18 20:33:49 +0100

    Fix GET/robots method path

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,10 @@
 import morgan from "morgan";
 import app from "./app.js";
-import endpointNotFound from "./middlewares/errors/endPointNotFound.js";
 import pingRouter from "../features/ping/router/pingRouter.js";
+import transformersRouter from "../features/transformer/router/transformersRouter.js";
+import endpointNotFound from "./middlewares/errors/endPointNotFound.js";
 
 app.use(morgan("dev"));
+app.use("/", transformersRouter);
 app.use("/", pingRouter);
 app.use(endpointNotFound);
