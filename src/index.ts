1d915a804eb9e23a7e664b68128281ed1fc94745
Ivan Aguilar <ivanaguiglez@gmail.com>
2023-11-18 15:58:47 +0100

    Connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import { connectToDatabase } from "./database/index.js";
 import { startServer } from "./server/app.js";
 import "./server/index.js";
 
 const port = process.env.PORT ?? 4000;
+
+if (!process.env.MONGODB_URL) {
+  console.log("Missing MongoDV Connection String");
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
+
 startServer(+port);
